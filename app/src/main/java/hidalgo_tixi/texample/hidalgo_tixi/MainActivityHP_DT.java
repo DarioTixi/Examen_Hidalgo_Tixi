42a31bdca80ccbc8a748dbeb9d59339aca98f0f5
dario <dario.moya2016@gmail.com>
2022-05-30 18:40:28 -0500

    Primer 6:30

---
+++ app/src/main/java/hidalgo_tixi/texample/hidalgo_tixi/MainActivityHP_DT.java
@@ -0,0 +1,13 @@
+package hidalgo_tixi.texample.hidalgo_tixi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityHP_DT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/hidalgo_tixi/texample/hidalgo_tixi/PrimerActividadTDHP.java
@@ -0,0 +1,13 @@
+package hidalgo_tixi.texample.hidalgo_tixi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrimerActividadTDHP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primer_actividad_tdhp);
+    }
+}
